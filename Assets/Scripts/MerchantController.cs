c323ae29700314b3fccd1e2c954f9a836e958da7
sangwonme <paulmoguri@naver.com>
2022-12-09 17:27:59 +0900

    :sparkles: vanishing merchant

--- Assets/Scripts/MerchantController.cs
+++ Assets/Scripts/MerchantController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class MerchantController : MonoBehaviour
 {
     private UIMerchant ui;
+    private Timer timer;
+    private float opacity;
+    public SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = GameObject.Find("GameController").GetComponent<Timer>();
         ui = GetComponent<UIMerchant>();
         ui.turnOffUI();
     }
@@ -16,11 +20,24 @@ public class MerchantController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // appear
+        if(timer.isDay){
+            if(opacity < 1.0f){
+                opacity += 0.01f;
+            }
+        }
+        // vanish
+        else{
+            if(opacity > 0.0f){
+                opacity -= 0.01f;
+            }
+            ui.setUIKey(false);
+        }
+        sprite.color = new Color(1.0f, 1.0f, 1.0f, opacity);
     }
 
     private void OnTriggerStay(Collider other) {
-        if(other.tag == "Player"){
+        if(other.tag == "Player" && timer.isDay){
             ui.setUIKey(true);
             if(Input.GetKeyDown(KeyCode.C)){
                 ui.setUIShop(true);
